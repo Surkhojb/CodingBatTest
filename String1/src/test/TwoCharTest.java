1c127dccc820f53bf130a33796f1cbe518e56e3c
Surkhojb <juanjo@ragecoders.com>
2018-02-22 12:31:10 +0100

    twochartest updated

--- String1/src/test/TwoCharTest.java
+++ String1/src/test/TwoCharTest.java
@@ -6,14 +6,13 @@ public class TwoCharTest {
 
     @Test
     public void should_return_a_string_with_lenght_2_starting_at_the_given_index(){
-        //assertEquals("ja",twoChar("java",0));
-        //assertEquals("ja",twoChar("java",3));
+        assertEquals("ja",twoChar("java",0));
+        assertEquals("ja",twoChar("java",3));
         assertEquals("va",twoChar("java",2));
         assertEquals("lo",twoChar("Hello",3));
     }
 
     private String twoChar(String str, int index){
-        System.out.println("String lenght " + str.length());
         if( (index + 2 > str.length()) || index < 0)
             return str.substring(0,2);
         else
